b23471b37dc31a6c200f265a184a9d4a1147892e
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-04-23 14:29:56 -0400

    finished

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ function SelectPage() {
             element.classList.add('active');
         }
     })
+
+    //toggle theme
+
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
 }
 
 SelectPage();
